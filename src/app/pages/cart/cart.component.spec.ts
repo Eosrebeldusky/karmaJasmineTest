eceb158d882f857e52d4e69c2188a3ee70764c8e
Eosrebeldusky <eosrebeldusky@gmail.com>
2022-11-26 10:52:13 -0300

    First Commit

---
+++ src/app/pages/cart/cart.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { CartComponent } from "./cart.component";
+
+describe('Cart component', ()=>{
+
+    let component : CartComponent;
+    let fixture : ComponentFixture<CartComponent>;
+
+
+    beforeEach(()=>{
+        TestBed.configureTestingModule({
+            imports:[],declarations:[],providers:[]
+
+
+        })
+
+
+    });
+
+
+
+
+});
